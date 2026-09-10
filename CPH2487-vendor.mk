5969dc457ad51862b2c5bcfaab1a89a0e52ea8df
Gokulgethu <gokulgethu30@gmail.com>
2026-09-10 02:49:29 +0000

    sync: adopt proven vendor blobs from rocko5498/android_vendor_oneplus_udon @ d2b7ad0e82 (post-20260727 NFC rebuild generation)

--- CPH2487-vendor.mk
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (C) 2023-2026 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-$(call inherit-product-if-exists, vendor/oneplus/udon/udon-vendor.mk)
-$(call inherit-product-if-exists, vendor/oneplus/CPH2487/udon-vendor.mk)
